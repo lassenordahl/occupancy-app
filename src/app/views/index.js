c35ba72c4318f9113e663a403b4dd9eb899f8a51
Lasse Nordahl <lasseanordahl@gmail.com>
2020-02-25 16:48:05 -0800

    quick fix index file

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -4,11 +4,6 @@ export { default as InvalidRoute } from './invalid-route/InvalidRoute';
 
 export { default as EntityInformation } from './entity-views/entity-information/EntityInformation';
 
-export { default as BuildingInformation } from './geo-filters/building-information/BuildingInformation';
-export { default as HistoricalGeo } from './geo-filters/historical-geo/HistoricalGeo';
-export { default as GlobalInformation } from './geo-filters/global-information/GlobalInformation';
-export { default as FloorInformation } from './geo-filters/floor-information/FloorInformation';
-
 export { default as FloorDialog } from './dialog-views/floor-dialog/FloorDialog';
 export { default as GlobalDialog } from './dialog-views/global-dialog/GlobalDialog';
 export { default as OccupancyDialog } from './dialog-views/occupancy-dialog/OccupancyDialog';
