c4b78ebbb837c65e173e66821b76f8dd7ee46e5d
Lasse Nordahl <lasseanordahl@gmail.com>
2020-03-25 15:26:16 -0700

    quick fix

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -4,8 +4,6 @@ export { default as InvalidRoute } from './invalid-route/InvalidRoute';
 
 export { default as EntityInformation } from './entity-views/entity-information/EntityInformation';
 
-export { default as FloorDialog } from './dialog-views/floor-dialog/FloorDialog';
-export { default as GlobalDialog } from './dialog-views/global-dialog/GlobalDialog';
 export { default as OccupancyDialog } from './dialog-views/occupancy-dialog/OccupancyDialog';
 
 export { default as PrivateRoute } from './authentication/Authentication';
