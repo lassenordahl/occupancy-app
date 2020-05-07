3f8b28c13201fc04c738de18cdec9c91182dc389
Tristan <tgjs.junk@gmail.com>
2020-05-07 22:16:45 +0000

    small tweak to proxy fix

--- backend_server/app.py
+++ backend_server/app.py
@@ -3,7 +3,7 @@ from src import create_app
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
-app = ProxyFix(app, x_for=1, x_host=1)
+app = ProxyFix(app)
 
 if __name__ == '__main__':
 	app.run()
