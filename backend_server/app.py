e61daecee28da3616e911e94b99d3ae2d2df4ea4
Tristan <tgjs.junk@gmail.com>
2020-05-07 22:14:47 +0000

    added backend proxy fix

--- backend_server/app.py
+++ backend_server/app.py
@@ -1,7 +1,9 @@
 import os
 from src import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = create_app()
+app = ProxyFix(app, x_for=1, x_host=1)
 
 if __name__ == '__main__':
 	app.run()
