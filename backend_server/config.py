a7f8e8709ef5b36d916b1fc95cf75f60ec8ec615
Tristan <tgjs.junk@gmail.com>
2020-05-10 02:50:53 +0000

    update config for redirect change

--- backend_server/config.py
+++ backend_server/config.py
@@ -29,7 +29,8 @@ class UCIConfig:
 	OAUTH_AUTHORIZE_URL = TIPPERS_OAUTH_BASE_URL+'/oauth2/authorize'
 	OAUTH_API_BASE_URL = TIPPERS_OAUTH_BASE_URL+'/api/2/'
 	SUBDIR_APP_NAME = 'occupancy'
-	FRONTEND_REDIRECT_URL = 'https://uci-tippers.ics.uci.edu/'+SUBDIR_APP_NAME
+	FRONTEND_BASE_URL = 'https://uci-tippers.ics.uci.edu'
+	FRONTEND_REDIRECT_URL = '{}/{}'.format(FRONTEND_BASE_URL, SUBDIR_APP_NAME)
 	FRONTEND_STATIC_FILES = '../../../build/static'
 	SUFFIX = 'uci_occupancy'
 	SESSION_COOKIE_NAME = 'session{}'.format(SUFFIX)
@@ -44,7 +45,8 @@ class HomeConfig:
 	OAUTH_AUTHORIZE_URL = TIPPERS_OAUTH_BASE_URL+'/oauth2/authorize'
 	OAUTH_API_BASE_URL = TIPPERS_OAUTH_BASE_URL+'/api/2/'
 	SUBDIR_APP_NAME = 'occupancy'
-	FRONTEND_REDIRECT_URL = 'https://home-tippers.ics.uci.edu/'+SUBDIR_APP_NAME
+	FRONTEND_BASE_URL = 'https://home-tippers.ics.uci.edu'
+	FRONTEND_REDIRECT_URL = '{}/{}'.format(FRONTEND_BASE_URL, SUBDIR_APP_NAME)
 	FRONTEND_STATIC_FILES = '../../../build/static'
 	SUFFIX = 'home_occupancy'
 	SESSION_COOKIE_NAME = 'session{}'.format(SUFFIX)
